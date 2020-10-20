0b73f8ee907ea7419626fb314a5150b29d90f61b
Ylli Abazi <ylli.abazi019@gmail.com>
2020-10-21 00:31:40 +0200

    Add profile model; add profile routes to get profile of logged in user, to create and to update a user

--- config/db.js
+++ config/db.js
@@ -4,7 +4,13 @@ const db = config.get('mongoURI');
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+    await mongoose.connect(db, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true, 
+      useCreateIndex: true, 
+      useFindAndModify: false
+    });
+    
     console.log('MongoDB connected!')
   } catch(err) {
     console.error(err.message);
