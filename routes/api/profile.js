6c8eb040d3b3a1b817b472b163aa70ba74d22b75
Ylli Abazi <ylli.abazi019@gmail.com>
2020-10-25 00:25:46 +0200

    Add Profile API Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const {check, validationResult} = require('express-validator');
+const request = require('request');
+const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -86,6 +88,200 @@ router.post('/', [auth, [
       res.status(500).send('Server error');
     }
     
+});
+
+// @route     GET api/profile
+// @desc      Get all profiles
+// @access    Public
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error({msg: error.message});
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get profile by user ID
+// @access    Public
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar']);
+    if(!profile) return res.status(400).json({msg: 'Profile not found'});
+    res.json(profile);
+  } catch (error) {
+    console.error({msg: error.message});
+    if(error.kind == 'ObjectId') {
+      return res.status(400).json({msg: 'Profile not found'})
+    }
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     DELETE api/profile
+// @desc      Delete profile, user & posts
+// @access    Private
+
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({user: req.user.id});
+    // Remove user
+    await User.findOneAndRemove({_id: req.user.id});
+
+    res.json({msg: 'User deleted'});
+    
+  } catch (error) {
+    console.error({msg: error.message});
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     PUT api/profile/experience
+// @desc      Add profile experience
+// @access    Private
+
+router.put('/experience', [auth, [
+    check('title', 'title is required').not().isEmpty(),
+    check('company', 'company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array()});
+  };
+
+  const {title, company, location, from, to, current, description} = req.body;
+  
+  const newExp = {
+    title, 
+    company, 
+    location, 
+    from, 
+    to, 
+    current,
+    description
+  };
+
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+    profile.experience.unshift(newExp);
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     DELETE api/profile/experience/:exp_id
+// @desc      Delete profile experience from profile
+// @access    Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+  
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+  
+    profile.experience.splice(removeIndex, 1);
+    
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+  });
+
+// @route     PUT api/profile/education
+// @desc      Add profile education
+// @access    Private
+router.put('/education', [auth, [
+  check('school', 'School is required').not().isEmpty(),
+  check('degree', 'Degree is required').not().isEmpty(),
+  check('fieldofstudy', 'Field of study is required').not().isEmpty(),
+  check('from', 'From date is required').not().isEmpty(),
+]
+], async (req, res) => {
+const errors = validationResult(req);
+if(!errors.isEmpty()) {
+  return res.status(400).json({errors: errors.array()});
+};
+
+const {school, degree, fieldofstudy,  from, to, current, description} = req.body;
+
+const newEdu = {
+  school, 
+  degree, 
+  fieldofstudy, 
+  from, 
+  to, 
+  current,
+  description
+};
+
+try {
+  const profile = await Profile.findOne({user: req.user.id});
+  profile.education.unshift(newEdu);
+  await profile.save();
+  res.json(profile);
+} catch (error) {
+  console.error(error.message);
+  res.status(500).send('Server error');
+}
+});
+
+// @route     DELETE api/profile/education/:edu_id
+// @desc      Delete profile education from profile
+// @access    Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({user: req.user.id});
+
+    // Get remove index
+    const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+    
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route     GET  api/profile/github/:username
+// @desc      Get user repos from github
+// @access    Public
+
+router.get('/github/:username', async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&
+      client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: {'user-agent': 'node.js'}
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+      if(response.statusCode !== 200) return res.status(404).json({msg: 'No github profile found'});
+
+      res.json(JSON.parse(body));
+    })
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 })
 
 module.exports = router;
