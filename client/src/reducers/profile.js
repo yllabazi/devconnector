a4dcc87dafd2c8f77408a4d89c00c88ef1cef328
Ylli Abazi <ylli.abazi019@gmail.com>
2020-12-26 14:50:00 +0100

    Add Profile Actions and Reducer & Profiles Component

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,6 +1,8 @@
 import {
 	CLEAR_PROFILE,
 	GET_PROFILE,
+	GET_PROFILES,
+	GET_REPOS,
 	PROFILE_ERROR,
 	UPDATE_PROFILE,
 } from '../actions/types';
@@ -23,6 +25,18 @@ export default function (state = initialState, action) {
 			return {
 				...state,
 				profile: payload,
+				loading: false,
+			};
+		case GET_PROFILES:
+			return {
+				...state,
+				profiles: payload,
+				loading: false,
+			};
+		case GET_REPOS:
+			return {
+				...state,
+				repos: payload,
 				loading: false,
 			};
 		case PROFILE_ERROR:
