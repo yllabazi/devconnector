b14d50113f76a262293eaceacaa4b8e01a785aee
Ylli Abazi <ylli.abazi019@gmail.com>
2021-01-15 08:27:35 +0000

    Add profile education display

--- client/src/components/profile/ProfileEducation.js
+++ client/src/components/profile/ProfileEducation.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import Moment from 'react-moment';
 
-const ProfileEducation = (props) => {
-	return <div></div>;
+const ProfileEducation = ({ profile: { education } }) => {
+	return (
+		<div class="profile-edu bg-white p-2">
+			<h2 class="text-primary">Education</h2>
+			{education.length > 0 ? (
+				<Fragment>
+					{education.map((edu) => {
+						return (
+							<div key={edu._id}>
+								<h3>{edu.school}</h3>
+								<p>
+									<Moment date={edu.from} format="MMM YYYY" /> -{' '}
+									{!edu.current ? (
+										<Moment date={edu.to} format="MMM YYYY" />
+									) : (
+										'Now'
+									)}
+								</p>
+								<p>
+									<strong>Degree:</strong> {edu.degree}
+								</p>
+								<p>
+									<strong>Field of Study:</strong> {edu.fieldofstudy}
+								</p>
+								{edu.description && (
+									<p>
+										<strong>Description:</strong>
+										{edu.description}
+									</p>
+								)}
+							</div>
+						);
+					})}
+				</Fragment>
+			) : (
+				<h4>No education credentials</h4>
+			)}
+		</div>
+	);
 };
 
-ProfileEducation.propTypes = {};
+ProfileEducation.propTypes = {
+	profile: PropTypes.object.isRequired,
+};
 
 export default ProfileEducation;
